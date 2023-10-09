9446f1421c608ac39bb7b26af81956ad88a1bea7
Jakob Schlichting <jakob.schlichting@mail.de>
2023-10-09 07:36:00 +0200

    feat: created proper 404 page

--- src/root.tsx
+++ src/root.tsx
@@ -22,7 +22,7 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body class="bg-neutral-400">
+      <Body class="bg-neutral-300">
         <SiteHeader />
         <Suspense>
           <ErrorBoundary>
--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,10 +2,11 @@ import { A } from "solid-start";
 
 export default function NotFound() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
-      </h1>
+    <main class="text-center mx-auto text-neutral-900 p-4">
+        <h1 class="max-6-xs text-6xl text-red-700 font-thin uppercase my-16">
+            &#9888; not found &#9888;
+        </h1>
+        <div>return to <A href="/">homepage</A></div>
     </main>
   );
 }
